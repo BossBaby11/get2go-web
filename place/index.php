d56a13b71da3fd068240ae5ccf697236de545956
Ryan <ryanfaiz2005@gmail.com>
2022-10-12 20:07:16 +0700

    Add article element to place/index.html

--- place/index.php
+++ place/index.php
@@ -16,8 +16,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nama Tempat | Get2Go</title>
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="/style/place.css">
+    <link rel="stylesheet" href="/style/place.tmp.css">
 </head>
 <body>
-    
+    <div class="centre">
+        <article>
+            <img src="<?= $row[9]; ?>" alt="">
+
+            <h1><?= $row[2]; ?> | <?= $row[1]; ?></h1>
+
+            <h2>Lokasi Tempat Wisata</h2>
+            <div><div><iframe width="100%" height="500" id="gmap_canvas" src="<?= $row[8]; ?>" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><br><style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}</style><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div></div>
+
+            <h2>Jam Operasional Tempat Wisata</h2>
+            <p>Jam Buka : <?= $row[5]; ?></p>
+            <p>Jam Tutup : <?= $row[6]; ?></p>
+
+            <h2>Harga Tiket Masuk</h2>
+            <p>HTM : Rp <?= $row[7]; ?></p>
+
+            <h2>Galeri Tempat Wisata</h2>
+            <img src="<?= $row[10]; ?>" alt="">
+            <img src="<?= $row[11]; ?>" alt="">
+            
+        </article>
+    </div>
 </body>
 </html>
