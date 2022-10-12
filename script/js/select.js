67b98193e6db1b3768270ba18453fdfca2aae9a2
BossBaby11 <brainstik69@gmail.com>
2022-10-12 08:25:51 +0700

    Remove optionshow() function

--- script/js/select.js
+++ script/js/select.js
@@ -6,14 +6,5 @@ function select() {
     selectobject.remove(); 
     document.getElementById("placeholder").style.visibility = "hidden"; 
     document.getElementById("placeholder").style.display = "none";
-}
-
-// var for option
-var option = document.getElementsByClassName("option");
-
-// show all option
-function optionshow() {
-    for(var i = 0; i < option.length; i++){
-        option[i].style.display = "inline";
-    }
+    optionshow();
 }
