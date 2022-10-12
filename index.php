57e36a70d3b8e0b4a1fc6e6803cfdacfb1da2c2e
Ryan <ryanfaiz2005@gmail.com>
2022-10-12 18:53:15 +0700

    Add favicon to home, destination, place page

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
         <link rel="stylesheet" href="/style/home.css">
         <link rel="stylesheet" href="/style/home.tmp.css">
         <title>Home | Get2Go</title>
