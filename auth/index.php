e2ce5cd0a389b83de40027012ede2a6ef08e747b
Ryan <ryanfaiz2005@gmail.com>
2022-10-15 08:12:42 +0700

    Change page title

--- auth/index.php
+++ auth/index.php
@@ -31,7 +31,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login | Kementerian Pariwisata</title>
+    <title>Login | Get2Go</title>
     <link rel="stylesheet" href="/style/login.css">
     <link rel="icon" href="/favicon.ico">
 </head>
