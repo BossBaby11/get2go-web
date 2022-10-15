8a861ca098780986f55b3926afecdc01cb9e672b
Ryan <ryanfaiz2005@gmail.com>
2022-10-15 08:08:30 +0700

    Add script to auth page

--- auth/index.php
+++ auth/index.php
@@ -1,3 +1,30 @@
+<?php 
+    session_start();
+    require "../script/conn.php";
+
+    if ( isset($_POST["login"])) {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        
+        $result = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'");
+
+        if (mysqli_num_rows($result) === 1) {
+            $row = mysqli_fetch_assoc($result);
+
+            if (password_verify($password, $row["password"])) {
+                $_SESSION["login"] = true;
+                $_SESSION["name"] = $_POST["username"];
+                header("Location: /admin");
+                exit;
+            }
+            
+        }
+
+        $error = true;
+
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
