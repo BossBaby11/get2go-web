6264cb042e3295570f12e546634b1b2ed1b97f2b
Ryan <ryanfaiz2005@gmail.com>
2022-10-15 10:18:42 +0700

    Change page title

--- admin/berhasil.php
+++ admin/berhasil.php
@@ -12,8 +12,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Berhasil Ditambah</title>
-    <link rel="stylesheet" href="/style/admin.css">
+    <title>Data Berhasil <?= $message; ?> | Get2Go</title>
     <link rel="icon" href="/favicon.ico">
 </head>
 <body>
