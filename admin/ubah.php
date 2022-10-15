c4d5c597efc2968ca1cbd4d065b7b57f934bf50e
Ryan <ryanfaiz2005@gmail.com>
2022-10-15 09:48:39 +0700

    Change website title

--- admin/ubah.php
+++ admin/ubah.php
@@ -40,7 +40,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah Data | Kementerian Pariwisata</title>
+    <title>Tambah Data | Get2Go</title>
     <link rel="stylesheet" href="/style/input.css">
     <link rel="icon" href="/favicon.ico">
 </head>
