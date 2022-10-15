952ab85d264bd06bd3482f2211dce21e89baede7
Ryan <ryanfaiz2005@gmail.com>
2022-10-15 08:07:21 +0700

    Add auth page

---
+++ auth/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | Kementerian Pariwisata</title>
+    <link rel="stylesheet" href="/style/login.css">
+    <link rel="icon" href="/favicon.ico">
+</head>
+<body>
+    <section>
+        <form action="" method="post">
+            <?php if( isset($error) ) :?>
+            <p style="color: red; font-style: italic;">Username / Password salah</p>
+            <?php endif ?>
+
+            <h3>Username</h3>
+            <input type="text" name="username" id="username" maxlength="20" required autofocus autocomplete="off">
+            <br><br>
+
+            <h3>Password</h3>
+            <input type="password" name="password" id="password"maxlength="20" required autocomplete="off" >
+            <br><br>
+
+            <button type="submit" name="login">Login</button>
+        </form>
+    </section>
+</body>
+</html>
