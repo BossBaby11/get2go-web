0b4704bfb3640515edfef2c75e80ade21d3e1d8a
Ryan <ryanfaiz2005@gmail.com>
2022-10-15 10:05:11 +0700

    Add feedback

--- admin/hapus.php
+++ admin/hapus.php
@@ -7,5 +7,8 @@
 
     $result = mysqli_query($conn, "DELETE FROM tempat WHERE id = $id");
 
-    
+    $_SESSION["type"] =  "data berhasil dihapus";
+
+    header("Location: /admin/berhasil.php");
+    exit;
 ?>
