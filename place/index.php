e0e79b1331729dcc5144f8d3bc9090b7615e22eb
Ryan Faiz Sanie <ryanfaiz2005@gmail.com>
2022-10-20 18:41:34 +0700

    Add meta description to Place Page

--- place/index.php
+++ place/index.php
@@ -13,6 +13,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Detail tempat wisata <?= $row[1]; ?>, yang berisi lokasi, jam operasional, dan galeri tempat wisata">
     <title>Nama Tempat | Get2Go</title>
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="/style/place.css">
