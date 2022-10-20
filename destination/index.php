db9470aa8b88435070f724e879acbd35777489de
Ryan Faiz Sanie <ryanfaiz2005@gmail.com>
2022-10-20 18:22:47 +0700

    Add meta description

--- destination/index.php
+++ destination/index.php
@@ -18,6 +18,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="List tempat wisata di daerah <?= $lokasi; ?> yang bisa anda kunjungi.">
     <title>Destinasi Wisata | Get2Go</title>
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="/style/destination.css">
